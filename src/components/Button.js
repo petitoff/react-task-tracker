fa74bcb3af7be6ac174fbfe11d11677e2f7eba50
petit <petitoff@outlook.com>
2021-09-10 19:12:20 +0200

    Create a button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,20 @@
-const Button = ({color, text}) => {
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
     return (
         <div>
-        <button style={{backgroundColor: color}} className='btn'>{text}</button> 
+        <button onClick = {onClick} style={{backgroundColor: color}} className='btn'>{text}</button> 
         </div>
     )
 }
 
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes ={
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
 export default Button
