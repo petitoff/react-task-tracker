d9d809adb922b0b90f568111d1ec6883c4a3fccb
petit <petitoff@outlook.com>
2021-09-10 19:02:53 +0200

    Creating a button and assigning an appropriate style.

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({color, text}) => {
+    return (
+        <div>
+        <button style={{backgroundColor: color}} className='btn'>{text}</button> 
+        </div>
+    )
+}
+
+export default Button
