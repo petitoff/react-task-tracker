f7698f72e5f68af1a188ecfa59a4702fd911ad34
petit <petitoff@outlook.com>
2021-09-13 11:56:53 +0200

    The application highlights the selected items after double-clicking.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import Tasks from "./components/Tasks";
 import { useState } from "react"
 
 const App = () => {
-  const [tasks, setTask] = useState([{ id: 1, text: "Doctor", day: "Feb 5th at 2:30pm" }])
+  const [tasks, setTask] = useState([{
+    id: 1,
+    text: "Doctor",
+    day: "Feb 5th at 2:30pm",
+    reminder: true
+  }])
 
   // Delete Task
   const deleteTask = (id) => {
     setTask(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTask(
+      tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task'}
     </div>
   );
 }
