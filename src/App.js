fefc471460078699ba2ce4a6ff2c8bc3a6c87a7c
petit <petitoff@outlook.com>
2021-09-13 11:41:04 +0200

    When you click on the x icon, the task is deleted.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import { useState } from "react"
 const App = () => {
   const [tasks, setTask] = useState([{ id: 1, text: "Doctor", day: "Feb 5th at 2:30pm" }])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
